d2255831e8bfa6405884ef5d562580373183bb73
Kevin Maurizi <kevin.maurizi@bzz.ch>
2023-08-30 11:36:57 +0200

    done

--- main.py
+++ main.py
@@ -10,14 +10,28 @@ prices = {
     # Add more prices as needed
 }
 
-def update_price(product_name, new_price):
-    # Update the price in the prices dictionary
-    pass
 
-def calculate_total():
-    # Calculate the total cost based on products and prices
-    pass
+def update_prices(products, new_prices):
+    updated_products = []
+    for product in products:
+        updated_product = product.copy()
+        product_name = updated_product['product']
+        if product_name in new_prices:
+            updated_product['price'] = new_prices[product_name]
+        updated_products.append(updated_product)
+    return updated_products
+
+
+def calculate_total(products):
+    total = 0
+    for product in products:
+        total += product['price'] * product['quantity']
+    return total
+
 
 if __name__ == '__main__':
-    # Test your functions here
-    pass
+    updated_products = update_prices(products, prices)
+    print("Ursprüngliche Produkte:", products)
+    print("Aktualisierte Produkte:", updated_products)
+    print("Gesamtpreis (alt):", calculate_total(products))
+    print("Gesamtpreis (neu):", calculate_total(updated_products))
--- main_test.py
+++ main_test.py
@@ -1,23 +1,26 @@
 import main
 
-def test_update_price():
+
+def test_update_prices():
     """Test if the function correctly updates the prices."""
     new_prices = {'Apple': 1.5, 'Banana': 0.9}
-    updated_list = main.update_price(main.products, new_prices)
+    updated_list = main.update_prices(main.products, new_prices)
     for item in updated_list:
         assert item['price'] == new_prices.get(item['product'], item['price']), \
             f"Expected {new_prices.get(item['product'], item['price'])}, but got {item['price']}."
 
+
 def test_original_list_unchanged():
     """Test if the original product list remains unchanged."""
     original_list = main.products.copy()
-    main.update_price(main.products, main.prices)
+    main.update_prices(main.products, main.prices)
     assert main.products == original_list, f"Expected the original list to remain unchanged, but got {products}."
+
 
 def test_calculate_total():
     """Test if the total cost is correctly calculated."""
     new_prices = {'Apple': 1.5, 'Banana': 0.9}
-    updated_list = main.update_price(main.products, new_prices)
+    updated_list = main.update_prices(main.products, new_prices)
     total_cost = main.calculate_total(updated_list)
     expected_cost = sum(item['quantity'] * new_prices.get(item['product'], item['price']) for item in updated_list)
     assert total_cost == expected_cost, f"Expected {expected_cost}, but got {total_cost}."
