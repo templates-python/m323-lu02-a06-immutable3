c27156f4a6c9cfd2cae1d5623d01e68cfe86752a
Kevin Maurizi <kevin.maurizi@bzz.ch>
2023-08-30 11:35:04 +0200

    done

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
-def calculate():
-    """
-    calculates the total
-    :return: None
-    """
-    price = 15.75
-    quantity = 3  # TODO change quantity to 5
-    print(f'Total: {price * quantity}')
+products = [
+    {'product': 'Apple', 'quantity': 5, 'price': 1.2},
+    {'product': 'Banana', 'quantity': 2, 'price': 0.8},
+    # Add more products as needed
+]
 
+prices = {
+    'Apple': 0.5,
+    'Banana': 0.3,
+    # Add more prices as needed
+}
+
+def update_price(product_name, new_price):
+    # Update the price in the prices dictionary
+    pass
+
+def calculate_total():
+    # Calculate the total cost based on products and prices
+    pass
 
 if __name__ == '__main__':
-    calculate()
+    # Test your functions here
+    pass
