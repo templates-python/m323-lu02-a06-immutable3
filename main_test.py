f632754ca1a5d170bff6a8793388d5c85a1e821b
Kevin Maurizi <kevin.maurizi@bzz.ch>
2024-09-30 09:03:19 +0200

    Ran pyink on branch main

--- main_test.py
+++ main_test.py
@@ -6,15 +6,18 @@ def test_update_price():
     new_prices = {'Apple': 1.5, 'Banana': 0.9}
     updated_list = main.update_prices(main.products, new_prices)
     for item in updated_list:
-        assert item['price'] == new_prices.get(item['product'], item['price']), \
-            f"Expected {new_prices.get(item['product'], item['price'])}, but got {item['price']}."
+        assert item['price'] == new_prices.get(
+            item['product'], item['price']
+        ), f"Expected {new_prices.get(item['product'], item['price'])}, but got {item['price']}."
 
 
 def test_original_list_unchanged():
     """Test if the original product list remains unchanged."""
     original_list = main.products.copy()
     main.update_prices(main.products, main.new_prices)
-    assert main.products == original_list, f"Expected the original list to remain unchanged, but got {products}."
+    assert (
+        main.products == original_list
+    ), f'Expected the original list to remain unchanged, but got {products}.'
 
 
 def test_calculate_total():
@@ -22,5 +25,10 @@ def test_calculate_total():
     new_prices = {'Apple': 1.5, 'Banana': 0.9}
     updated_list = main.update_prices(main.products, new_prices)
     total_cost = main.calculate_total(updated_list)
-    expected_cost = sum(item['quantity'] * new_prices.get(item['product'], item['price']) for item in updated_list)
-    assert total_cost == expected_cost, f"Expected {expected_cost}, but got {total_cost}."
+    expected_cost = sum(
+        item['quantity'] * new_prices.get(item['product'], item['price'])
+        for item in updated_list
+    )
+    assert (
+        total_cost == expected_cost
+    ), f'Expected {expected_cost}, but got {total_cost}.'
